02387e3d7c97f9b63a63724171c8b1b1cb325025
JorgeFumagalli <jorgefumagalli@yahoo.com.br>
2025-10-07 18:46:49 -0300

    Update 01_extraction.py
    Removal of redundant variables

--- 01_extraction.py
+++ 01_extraction.py
@@ -38,11 +38,6 @@ SERIES = {
     "comprometimento_renda": 29034,  # Comprometimento de renda das famílias com o serviço da dívida com o Sistema Financeiro Nacional - Com ajuste sazonal
     "endividamento_familias": 29037,  # Endividamento das famílias com SFN (%) - Até 2021
     "inadimplencia_familias": 21082,  # Inadimplência das famílias (%)
-    
-    # Componentes de Renda (para análise complementar)
-    "servico_divida": 21080,     # Serviço da dívida das famílias (R$ milhões)
-    "renda_disponivel": 21081,   # Renda disponível (R$ milhões)
-}
 
 START = "2015-01-01"
 END   = "2025-07-01"
